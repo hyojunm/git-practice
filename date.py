efdff25650ed11296ab0ee6c4c039a022f1db22e
Jash Bhalani <jashb2017@gmail.com>
2020-09-12 12:15:17 -0500

    Added Current Time Message

--- date.py
+++ date.py
@@ -5,4 +5,10 @@ import time
 date = time.strftime("%A, %B %d, %Y")
 message = f"Today is {date}"
 
+
 print(message)
+
+
+clock = time.localtime()
+current_time = time.strftime("%H:%M:%S", t)
+print(current_time)
